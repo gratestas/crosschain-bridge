cfc7984a0dfeb7a657ff8cfec6f7ff281d10f262
gratestas <stasgrate@gmail.com>
2022-09-21 13:28:38 +0300

    add storage to support upgrade functionality

---
+++ contracts/storage/UpgradabilityStorage.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+contract UpgradabilityStorage {
+    uint256 internal _version;
+    address internal _implementation;
+
+    function version() external view returns (uint256) {
+        return _version;
+    }
+
+    function implementation() external view returns (address) {
+        return _implementation;
+    }
+}
