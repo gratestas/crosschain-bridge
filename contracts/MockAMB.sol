d27cb558b14930e926e92b7e920b130a6a6ffcaa
gratestas <stasgrate@gmail.com>
2022-10-05 15:21:22 +0300

    refactor: add interface inheritance

--- contracts/MockAMB.sol
+++ contracts/MockAMB.sol
@@ -1,7 +1,9 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-contract MockAMB {
+import {IAMB} from "./interfaces/IAMB.sol";
+
+contract MockAMB is IAMB {
     uint256 private _currentMessageId;
     address private _currentMessageSender;
 
